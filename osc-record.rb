4d45f7c2b9746220beeb98d646e9412b0c861ea6
Daniel Brodie <11337994+danielbrodie@users.noreply.github.com>
2026-03-14 19:36:50 -0400

    osc-record v1.0.7: fix TUI deadlock (sendToUI goroutines blocked pre-launch)

--- osc-record.rb
+++ osc-record.rb
@@ -1,15 +1,15 @@
 class OscRecord < Formula
   desc "OSC-triggered video capture for live production"
   homepage "https://github.com/danielbrodie/osc-record"
-  version "1.0.6"
+  version "1.0.7"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/danielbrodie/osc-record/releases/download/v1.0.6/osc-record_darwin_arm64.tar.gz"
-      sha256 "ca6e50f3fa9087eb2b1a22f3bfbc17e2772731e9b9022b99e26c55000c86ef37"
+      url "https://github.com/danielbrodie/osc-record/releases/download/v1.0.7/osc-record_darwin_arm64.tar.gz"
+      sha256 "df911e4a398b0448317c3083444c16dcf567a7a9a8cf7c268c159faf0323bb74"
     else
-      url "https://github.com/danielbrodie/osc-record/releases/download/v1.0.6/osc-record_darwin_amd64.tar.gz"
-      sha256 "5a44efe9e0916556f9e88e844db8268dc98688d2f0eb64d36263c54469d53100"
+      url "https://github.com/danielbrodie/osc-record/releases/download/v1.0.7/osc-record_darwin_amd64.tar.gz"
+      sha256 "6887be5ece1398c0eb310b7eb76a1a39ee1953de3a0ddab58c78b33ef5b46ae2"
     end
   end
 
