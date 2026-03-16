2193f0a229170b9f90cfd2ca3d78613e97f3671d
Daniel Brodie <11337994+danielbrodie@users.noreply.github.com>
2026-03-16 18:24:30 -0400

    osc-record v1.3.14: fix path quote stripping

--- osc-record.rb
+++ osc-record.rb
@@ -1,15 +1,15 @@
 class OscRecord < Formula
   desc "OSC-triggered video capture for live production"
   homepage "https://github.com/danielbrodie/osc-record"
-  version "1.3.13"
+  version "1.3.14"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/danielbrodie/osc-record/releases/download/v1.3.13/osc-record_darwin_arm64.tar.gz"
-      sha256 "6cc5bcf83877f8aaadd2c09075e352efe4c6ad9b1a43366aa36948b0f1597e2c"
+      url "https://github.com/danielbrodie/osc-record/releases/download/v1.3.14/osc-record_darwin_arm64.tar.gz"
+      sha256 "8dca8ab6f8316a2053d8444c65fe96fbcd8f06b4d6291e8201b050f52d4357e9"
     else
-      url "https://github.com/danielbrodie/osc-record/releases/download/v1.3.13/osc-record_darwin_amd64.tar.gz"
-      sha256 "5fa54efc5c3d867026976e49153d7216db70bc624b30fa7fe4585b66915f3eb1"
+      url "https://github.com/danielbrodie/osc-record/releases/download/v1.3.14/osc-record_darwin_amd64.tar.gz"
+      sha256 "9104dce35b2461741701a2863935589ba870c438eb984981c2a83e23615a4b86"
     end
   end
 
