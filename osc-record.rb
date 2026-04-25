8b32deccbe4a22a1e6e4c8dfc78179b94ce3a2e3
Daniel Brodie <11337994+danielbrodie@users.noreply.github.com>
2026-04-25 05:18:39 -0600

    v1.5.0

--- osc-record.rb
+++ osc-record.rb
@@ -1,11 +1,11 @@
 class OscRecord < Formula
   desc "OSC-triggered video capture for live production"
   homepage "https://github.com/danielbrodie/osc-record"
-  version "1.4.6"
+  version "1.5.0"
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/danielbrodie/osc-record/releases/download/v1.4.6/osc-record_darwin_arm64.tar.gz"
-      sha256 "97842c48938c7a387b53b811c1a05c8d21389eb3a28c52876987c4dd8bf14404"
+      url "https://github.com/danielbrodie/osc-record/releases/download/v1.5.0/osc-record_darwin_arm64.tar.gz"
+      sha256 "001278802922fef8410abdcef0cb55460780dcad4500fafe30ee7ca06a14998b"
     end
   end
   depends_on "ffmpeg"
